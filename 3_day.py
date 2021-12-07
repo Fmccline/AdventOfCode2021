14b50dbbafbe95bdf5ca5135c53d88ba7e202418
Fmccline <frankmcline@gmail.com>
2021-12-06 18:33:34 -0800

    Refactored day 3 to use base class

--- 3_day.py
+++ 3_day.py
@@ -1,80 +1,82 @@
-import util
-INPUT_FILE = 'day3_input.txt'
-TEST_DATA = ['00100','11110','10110','10111','10101','01111','00111','11100','10000','11001','00010','01010']
+from typing import List
+from aoc_day import AoCDay
 
 
-def get_bit_count(bits_list):
-    bit_count = [0 for _ in range(len(bits_list[0]))]
-    for bits in bits_list:
-        idx = 0
-        for bit in bits:
-            bit_count[idx] += 1 if bit == '1' else -1
-            idx += 1
-    return bit_count
+class AoCDay3(AoCDay):
+
+    def __init__(self) -> None:
+        super().__init__(3)
+        self.bits_list = []
+
+    def setup_data(self, data) -> List:
+        self.bits_list = data
+        return data
+
+    def solve_part_one(self, data) -> None:
+        return self.get_power_consumption_rate()
+
+    def solve_part_two(self, data) -> None:
+        return self.get_life_support_rating()
+
+    def get_power_consumption_rate(self):
+        bit_count = self.get_bit_count()
+        gamma_rate = self.get_gamma_rate(bit_count)
+        epsilon_rate = self.get_epsilon_rate(gamma_rate)
+        consumption_rate = int(epsilon_rate, 2) * int(gamma_rate, 2)
+        print(f'Epsilon * Gamma = {consumption_rate}')
+        return consumption_rate
+
+    def get_bit_count(self):
+        bits_list = self.bits_list
+        bit_count = [0 for _ in range(len(bits_list[0]))]
+        for bits in bits_list:
+            idx = 0
+            for bit in bits:
+                bit_count[idx] += 1 if bit == '1' else -1
+                idx += 1
+        return bit_count
 
 
-def get_gamma_rate(bit_count):
-    bits = ''
-    for count in bit_count:
-        if count < 0:
-            bits += '0'
+    def get_gamma_rate(self, bit_count):
+        bits = ''
+        for count in bit_count:
+            if count < 0:
+                bits += '0'
+            else:
+                bits += '1'
+        return bits
+
+
+    def get_epsilon_rate(self, gamma_rate):
+        eps_rate = ''
+        for c in gamma_rate:
+            eps_rate += '1' if c == '0' else '0'
+        return eps_rate
+
+
+    def get_gas_rating(self, is_oxygen):
+        gas_rating = 0
+        common_bits = self.bits_list
+        for bit_idx in range(len(common_bits[0])):
+            common_bits = self.get_common_bits(common_bits, bit_idx, is_oxygen)
+            if len(common_bits) == 1:
+                gas_rating = int(common_bits[0], 2)
+                break
+        return gas_rating
+
+
+    def get_life_support_rating(self):
+        oxy_rating = self.get_gas_rating(True)
+        co2_rating = self.get_gas_rating(False)
+        return oxy_rating*co2_rating
+
+
+    def get_common_bits(self, bits_list, bit_idx, is_oxygen):
+        common_bits = {'0': [], '1': []}
+        for bits in bits_list:
+            common_bits[bits[bit_idx]].append(bits)
+        if is_oxygen:
+            return common_bits['0'] if len(common_bits['1']) < len(common_bits['0']) else common_bits['1']
         else:
-            bits += '1'
-    return bits
+            return common_bits['1'] if len(common_bits['1']) < len(common_bits['0']) else common_bits['0']
 
-
-def get_epsilon_rate(gamma_rate):
-    eps_rate = ''
-    for c in gamma_rate:
-        eps_rate += '1' if c == '0' else '0'
-    return eps_rate
-
-
-def get_gas_rating(bits_list, is_oxygen):
-    gas_rating = 0
-    common_bits = bits_list
-    for bit_idx in range(len(bits_list[0])):
-        common_bits = get_common_bits(common_bits, bit_idx, is_oxygen)
-        if len(common_bits) == 1:
-            gas_rating = int(common_bits[0], 2)
-            break
-    return gas_rating
-
-
-def get_life_support_rating(bits_list):
-    oxy_rating = get_gas_rating(bits_list, True)
-    co2_rating = get_gas_rating(bits_list, False)
-    return oxy_rating, co2_rating, oxy_rating*co2_rating
-
-
-def get_common_bits(bits_list, bit_idx, is_oxygen):
-    common_bits = {'0': [], '1': []}
-    for bits in bits_list:
-        common_bits[bits[bit_idx]].append(bits)
-    if is_oxygen:
-        return common_bits['0'] if len(common_bits['1']) < len(common_bits['0']) else common_bits['1']
-    else:
-        return common_bits['1'] if len(common_bits['1']) < len(common_bits['0']) else common_bits['0']
-
-
-
-if __name__ == '__main__':
-    is_test = False
-    data = TEST_DATA
-    if not is_test:  
-        data = util.read_input(INPUT_FILE)
-
-    bit_count = get_bit_count(data)
-    gamma_rate = get_gamma_rate(bit_count)
-    epsilon_rate = get_epsilon_rate(gamma_rate)
-    print('Epsilon * Gamma = ' + str(int(epsilon_rate, 2) * int(gamma_rate, 2)))
-    print(f'(Oxygen rating, CO2 rating, Life support rating) : {get_life_support_rating(data)}')
-    
-    if is_test:
-        print(bit_count)
-        print("Gamma")
-        print(gamma_rate)
-        print(int(gamma_rate, 2))
-        print("Epsilon")
-        print(epsilon_rate)
-        print(int(epsilon_rate, 2))
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import argparse
+day3 = __import__('3_day')
 day4 = __import__('4_day')
 
 
+
 TEST_CASES = {
+    3: (day3.AoCDay3(), (198, 230), (3959450, 7440311)),
     4: (day4.AoCDay4(), (4512, 1924), (44088, 23670))
 }
 
@@ -24,6 +27,7 @@ if day_num is not None:
     days = [day_num]
 
 ASSERTION_IDX = 1 if is_test else 2
+assertions = [0,0]
 for day in days:
     if day in TEST_CASES.keys():
         aoc_day = TEST_CASES[day][0]
@@ -31,5 +35,18 @@ for day in days:
         if should_assert:
             ans1, ans2 = TEST_CASES[day][ASSERTION_IDX]
             print(f'Day {day}')
-            print(f'{part1 == ans1}: {part1} == {ans1} (Actual / Expected)')
+            print(f'Part 1: [{part1 == ans1}] {part1} == {ans1} (Actual / Expected)')
+            print(f'Part 2: [{part2 == ans2}] {part2} == {ans2} (Actual / Expected)')
+            assertions[0] += 2
+            if ans1 == part1:
+                assertions[1] += 1
+            if ans2 == part2:
+                assertions[1] += 1
+
+if should_assert:
+    print('\nDone asserting.')
+    print(f'Correct: {assertions[1]}/{assertions[0]}')
+    print(f'Incorrect: {assertions[0] - assertions[1]}\n')
+
+            
     
