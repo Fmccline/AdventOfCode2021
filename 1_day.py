038160b9652a7404ba20c207d9340fe7c79d076e
Fmccline <frankmcline@gmail.com>
2021-12-06 18:53:03 -0800

    Refactored day 1

--- 1_day.py
+++ 1_day.py
@@ -1,42 +1,61 @@
-import util
+from aoc_day import AoCDay 
 DAY_1_FILE = 'day1_input.txt'
 test_data = [199,200,208,210,200,207,240,269,260,263]
 
-# Part 1
-def count_inc_depths(depths):
-    if len(depths) < 2:
-        return 0
 
-    last = depths[0]
-    num = 0
+class AoCDay1(AoCDay):
 
-    for depth in depths[1:]:
-        if depth > last:
-            num += 1
-        last = depth
-    return num
+    def __init__(self) -> None:
+        super().__init__(1)
+        self.depths = []
+
+    def setup_data(self, data):
+        for str_num in data:
+            self.depths.append(int(str_num))
+        return data
+
+    def solve_part_one(self, data):
+        return self.count_inc_depths()
+
+    def solve_part_two(self, data):
+        return self.sliding_window_depths()
+
+    # Part 1
+    def count_inc_depths(self):
+        depths = self.depths
+        if len(depths) < 2:
+            return 0
+
+        last = depths[0]
+        num = 0
+
+        for depth in depths[1:]:
+            if depth > last:
+                num += 1
+            last = depth
+        return num
+
+    # Part 2
+    def sliding_window_depths(self):
+        depths = self.depths
+        if len(depths) < 2:
+            return 0
+        
+        inc = 0
+        for idx in range(0, len(depths) - 3):
+            curr = depths[idx]
+            next = depths[idx + 3]
+            if curr < next:
+                inc += 1
+        return inc
 
 
-# Part 2
-def sliding_window_depths(depths):
-    if len(depths) < 2:
-        return 0
-    
-    inc = 0
-    for idx in range(0, len(depths) - 3):
-        curr = depths[idx]
-        next = depths[idx + 3]
-        if curr < next:
-            inc += 1
-    return inc
+# if __name__ == '__main__':
+#     testing = False
+#     depths = test_data
+#     if not testing:
+#         depths = util.read_input(DAY_1_FILE)
+#         for idx in range(len(depths)):
+#             depths[idx] = int(depths[idx])
 
-
-if __name__ == '__main__':
-    testing = False
-    depths = test_data
-    if not testing:
-        depths = util.read_input(DAY_1_FILE)
-        for idx in range(len(depths)):
-            depths[idx] = int(depths[idx])
-
-    print(sliding_window_depths(depths))
+#     print(sliding_window_depths(depths))
