0652899891c07687ed6f4c8a510d59cfba85d5f6
Fmccline <frankmcline@gmail.com>
2021-12-15 05:04:24 -0800

    Finished part 2 of day 10

--- 10_day.py
+++ 10_day.py
@@ -14,7 +14,7 @@ class AoCDay10(AoCDay):
         return self.get_corrupted_score()
 
     def solve_part_two(self):
-        return None
+        return self.get_auto_complete_score()
 
     def get_corrupted_score(self):
         SCORES = {')': 3, ']': 57, '}': 1197, '>': 25137}
@@ -32,4 +32,34 @@ class AoCDay10(AoCDay):
                         score += SCORES[c]
                         break
         return score
-                    
+
+    def get_auto_complete_score(self):
+        SCORES = {')': 1, ']': 2, '}': 3, '>': 4}
+        OPEN_CHARS = set(['(', '[', '{', '<'])
+        CLOSE_TO_OPEN = {')': '(', ']': '[', '}': '{', '>': '<'}
+        OPEN_TO_CLOSE = {}
+        for key, value in CLOSE_TO_OPEN.items():
+            OPEN_TO_CLOSE[value] = key
+        scores = []
+
+        for line in self.lines:
+            opens = []
+            is_corrupt = False
+            for c in line:
+                if c in OPEN_CHARS:
+                    opens.append(c)
+                else:
+                    inv_c = opens.pop()
+                    if CLOSE_TO_OPEN[c] != inv_c:
+                        is_corrupt = True
+                        break
+            if not is_corrupt:
+                score = 0
+                while opens:
+                    c = opens.pop()
+                    inv_c = OPEN_TO_CLOSE[c]
+                    score = score*5 + SCORES[inv_c]
+                scores.append(score)
+
+        # return middle score
+        return sorted(scores)[int(len(scores)/2)]
